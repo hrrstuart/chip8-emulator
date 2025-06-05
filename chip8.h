6cd12f59e13f947465b33fb4553e7935dd17ae94
hrrstuart <stuartharry0313@gmail.com>
2025-06-05 14:33:24 +0100

    refactor(chip8.h): replace standard unsigned types with fixed-width uint types

--- chip8.h
+++ chip8.h
@@ -1,13 +1,16 @@
+#include <stdint.h>
 #ifndef CHIP8_H
 #define CHIP8_H
 
 typedef struct {
-    unsigned char memory[4096];
-    unsigned char V[16];
-    unsigned short pc;
-    unsigned short I;
-    unsigned short stack[16];
-    unsigned char display[64*32];
+    uint8_t memory[4096];
+    uint16_t V[16];
+    uint16_t pc;
+    uint16_t I;
+    uint16_t stack[16];
+    uint8_t display[64*32];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
 } Chip8;
 
 int chip8_init(Chip8* chip8);
