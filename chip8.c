64d55d2fcc0f5bfceb243d50d0bfdd8745208f85
hrrstuart <stuartharry0313@gmail.com>
2025-06-07 00:05:13 +0100

    feat: add 0xE instruction for keyboard pressing

--- chip8.c
+++ chip8.c
@@ -3,6 +3,37 @@
 #include <SDL2/SDL.h>
 #include "chip8.h"
 
+// CHIP-8 key mapping: maps 0x0-0xF to SDL scancodes
+// e.g. QWERTY keyboard 1 <=> CHIP-8 keyboard 1
+// QWERTY V <=> CHIP-8 F
+const SDL_Scancode chip8_keymap[16] = {
+    SDL_SCANCODE_X, // 0
+    SDL_SCANCODE_1, // 1
+    SDL_SCANCODE_2, // 2
+    SDL_SCANCODE_3, // 3
+    SDL_SCANCODE_Q, // 4
+    SDL_SCANCODE_W, // 5
+    SDL_SCANCODE_E, // 6
+    SDL_SCANCODE_A, // 7
+    SDL_SCANCODE_S, // 8
+    SDL_SCANCODE_D, // 9
+    SDL_SCANCODE_Z, // A
+    SDL_SCANCODE_C, // B
+    SDL_SCANCODE_4, // C
+    SDL_SCANCODE_R, // D
+    SDL_SCANCODE_F, // E
+    SDL_SCANCODE_V // F
+};
+
+// Returns 1 if the CHIP-8 key 'key' is pressed, 0 otherwise
+int is_chip8_key_pressed(uint8_t key) {
+    const Uint8* keystate = SDL_GetKeyboardState(NULL);
+    if (key < 16) {
+        return keystate[chip8_keymap[key]];
+    }
+    return 0;
+}
+
 int load_rom(Chip8* chip8, char* filename) {
     FILE* program = fopen(filename, "rb");
     if (!program) return 1;
@@ -200,6 +231,19 @@ int decode_and_execute(uint16_t instruction, Chip8* chip8) {
         case 0xD:
             configure_display(chip8, x, y, n);
             chip8->draw_flag = 1;
+            break;
+        case 0xE:
+            uint8_t key = chip8->V[x];
+            switch (nn) {
+                case 0x9E:
+                    if (is_chip8_key_pressed(key))
+                        chip8->pc += 2;
+                    break;
+                case 0xA1:
+                    if (!is_chip8_key_pressed(key))
+                        chip8->pc += 2;
+                    break;
+            }
             break;
     }
     return 0;
