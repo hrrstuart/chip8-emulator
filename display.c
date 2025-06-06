eef4bd45c5bf2ece6a65a97265341b8d0d8d2a2a
hrrstuart <stuartharry0313@gmail.com>
2025-06-06 17:29:58 +0100

    feat: draw chip8 display array to an output window

---
+++ display.c
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+#include "chip8.h"
+#define SCALE 10
+
+void draw_display(Chip8* chip8, SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for (int i = 0; i < sizeof(chip8->display); i++) {
+        if (chip8->display[i]) {
+            int x = i % 64;
+            int y = i / 64;
+            SDL_Rect rect = { x*SCALE, y*SCALE, SCALE, SCALE };
+            SDL_RenderFillRect(renderer, &rect);
+        }   
+    }
+
+    SDL_RenderPresent(renderer);
+}
