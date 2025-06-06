5bb367a629ffe53efe607363cb9965607ef20a5b
hrrstuart <stuartharry0313@gmail.com>
2025-06-06 23:43:06 +0100

    feat: add timer updating code

--- main.c
+++ main.c
@@ -16,9 +16,11 @@ int main(int argc, char *argv[]) {
     SDLContext sdl_context;
     open_window(&sdl_context);
 
-    LARGE_INTEGER freq, start, now;
+    LARGE_INTEGER freq, start, now, timer_last;
     QueryPerformanceFrequency(&freq);
     QueryPerformanceCounter(&start);
+    timer_last = start;
+
     int i = 0;
     while (1) {
         chip8_cycle(&chip8);
@@ -28,9 +30,19 @@ int main(int argc, char *argv[]) {
             chip8.draw_flag = 0;
         }
 
+        // 60 Hz timer update
+        LARGE_INTEGER timer_now;
+        QueryPerformanceCounter(&timer_now);
+        double timer_elapsed = (double)(timer_now.QuadPart - timer_last.QuadPart) * 1000 / freq.HighPart;
+        if (timer_elapsed >= 16.67) {
+            if (chip8.delay_timer > 0) chip8.delay_timer--;
+            if (chip8.sound_timer > 0) chip8.sound_timer--;
+            timer_last = timer_now;
+        }
+
+        // 700 Hz instruction timing
         QueryPerformanceCounter(&now);
         double elapsed = (double)(now.QuadPart - start.QuadPart) * 1000.0 / freq.QuadPart;
-
         if (elapsed < 1.42857) {
             Sleep(0);
             continue;
